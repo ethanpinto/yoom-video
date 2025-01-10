a09f5e5ce71d96d3f8c5be74bc5a0c32ea05ae6d
ethanpinto <103467034+ethanpinto@users.noreply.github.com>
2025-01-10 12:58:52 +1000

    Created structure of video call website. Using next.js

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-between fixes z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href="/" className="flex items-center gap-1">
+
+      </Link>
+
+    </nav>
+  )
+}
+
+export default Navbar
