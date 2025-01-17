63c7a89a5a5f28d2e5be4d693d75e7ba21aca50c
ethanpinto <103467034+ethanpinto@users.noreply.github.com>
2025-01-17 17:04:42 +1000

    Finished basic UI.

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,13 +1,13 @@
+"use client"
+
 import React from 'react'
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-} from "@/components/ui/sheet"
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import Image from 'next/image'
+import Link from 'next/link'
+import { sidebarLinks } from '@/constants'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
+import { DialogTitle } from '@radix-ui/react-dialog'
 
 
 /**
@@ -18,6 +18,7 @@ import Image from 'next/image'
  * @returns {JSX.Element} The rendered MobileNav component.
  */
 const MobileNav = () => {
+    const pathname = usePathname()
     return (
         <section className='w-full max-w-[264px]'>
             <Sheet>
@@ -30,14 +31,50 @@ const MobileNav = () => {
                         className='cursor-pointer sm:hidden'
                     />
                 </SheetTrigger>
-                <SheetContent>
-                    <SheetHeader>
-                        <SheetTitle>Are you absolutely sure?</SheetTitle>
-                        <SheetDescription>
-                            This action cannot be undone. This will permanently delete your account
-                            and remove your data from our servers.
-                        </SheetDescription>
-                    </SheetHeader>
+                <SheetContent side="left" className='border-none bg-dark-1'>
+                    <DialogTitle className="sr-only">Navigation Menu</DialogTitle>
+                    <Link href="/" className="flex items-center gap-1">
+                        <Image
+                            src="/icons/logo.svg"
+                            width={32}
+                            height={32}
+                            alt="Yoom logo"
+                            className='max-sm:size-10'
+                        />
+                        <p className='text-[26px] font-extrabold text-white'>
+                            Yoom
+                        </p>
+                    </Link>
+                    <div className='flex h-[calc(100vh-72px)]
+                    flex-col justify-between overflow-y-auto'></div>
+                    <SheetClose asChild>
+                        <section className='flex h-full flex-col
+                        gap-6 pt-16 text-white'>
+                            {sidebarLinks.map((link) => {
+                                const isActive = pathname === link.route || pathname.startsWith(link.route);
+
+                                return (
+                                    <Link href={link.route}
+                                        key={link.label}
+                                        className={cn('flex gap-4 items-center p-4 rounded-lg w-full max-w-60', {
+                                            'bg-blue-1': isActive,
+                                        })}
+                                    >
+                                        <Image
+                                            src={link.imgUrl}
+                                            alt={link.label}
+                                            width={20}
+                                            height={20}
+                                        />
+                                        <p className='font-semibold'>
+                                            {link.label}
+                                        </p>
+                                    </Link>
+                                )
+                            })}
+                        </section>
+                    </SheetClose>
+
                 </SheetContent>
             </Sheet>
         </section>
